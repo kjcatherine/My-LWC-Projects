e8639bc70f5a8af15e8221d7f4bab09603b62316
kate <katejohn967@gmail.com>
2022-11-24 14:43:46 +0100

    loaded fontawesome library as a static resource

--- force-app/main/default/lwc/memoryGameLwc/memoryGameLwc.js
+++ force-app/main/default/lwc/memoryGameLwc/memoryGameLwc.js
@@ -1,3 +1,21 @@
 import { LightningElement } from 'lwc';
-
-export default class MemoryGameLwc extends LightningElement {}
+//foontawesome is a css style so we use this
+import {loadStyle} from 'lightning/platformResourceLoader'
+import fontawesome from '@salesforce/resourceUrl/fontawesome'
+export default class MemoryGameLwc extends LightningElement {
+    isLibLoaded = false;
+    //Whenever we load a third party library we always use a renderedcallbacl\k
+    renderedCallback(){
+        if(this.isLibLoaded){
+            return
+        }else{
+            loadStyle(this, fontawesome+'/fontawesome/css/font-awesome.min.css')
+            .then(()=>{
+                console.log("Loaded Sucessfully")
+            }).catch(error =>{
+                console.error("error")
+            })
+            this.isLibLoaded = true
+        }
+    }
+}
