8f84b5f87135aac1b3896538427ab9bea607c5f5
kate <katejohn967@gmail.com>
2023-04-23 15:36:45 +0100

    moviesListApp using a movies api to display data in salesforce

---
+++ force-app/main/default/lwc/movieListApp/movieListApp.js
@@ -0,0 +1,38 @@
+import { LightningElement, track } from 'lwc';
+
+export default class MovieListApp extends LightningElement {
+    @track moviesData;
+    // query=''
+    // timer
+    connectedCallback() {
+        this.fetchMovieData()
+    }
+
+    fetchMovieData(){
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=78979c75fb54c40e6eeca19518342eda&language=en-US')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log("Api Response from tmdb", data);
+                this.moviesData = data.results.map(movie => ({
+                    ...movie,
+                    imageUrl: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                }));
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+    }
+
+    // fetchMoviesHandler(event){
+    //     this.query= event.target.value
+    //     window.clearTimeout(this.timer)
+    //     this.timer = setTimeout(()=>{
+    //         this.fetchMovieData()
+    //     }, 1000)
+    // }
+}
