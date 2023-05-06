fdac34650fce9c7c193785abea945fc035a5fc07
kate <katejohn967@gmail.com>
2023-05-06 12:14:59 +0100

    Working with maps in lwc

--- force-app/main/default/lwc/movieListApp/movieListApp.js
+++ force-app/main/default/lwc/movieListApp/movieListApp.js
@@ -1,16 +1,14 @@
 import { LightningElement, track } from 'lwc';
-
 export default class MovieListApp extends LightningElement {
     @track moviesData;
-    @track allMoviesData;
-    @track query = '';
+    @track searchQuery = '';
 
     connectedCallback() {
-        this.fetchAllMovies();
+        this.fetchMovieData();
     }
 
-    fetchAllMovies() {
-        fetch('https://api.themoviedb.org/3/movie/popular?api_key=78979c75fb54c40e6eeca19518342eda&language=en-US')
+    fetchMovieData() {
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=78979c75fb54c40e6eeca19518342eda&language=en-US`)
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
@@ -19,11 +17,10 @@ export default class MovieListApp extends LightningElement {
             })
             .then(data => {
                 console.log("Api Response from tmdb", data);
-                this.allMoviesData = data.results.map(movie => ({
+                this.moviesData = data.results.map(movie => ({
                     ...movie,
                     imageUrl: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
                 }));
-                this.moviesData = this.allMoviesData;
             })
             .catch(error => {
                 console.error('There was a problem with the fetch operation:', error);
@@ -31,9 +28,15 @@ export default class MovieListApp extends LightningElement {
     }
 
     fetchMoviesHandler(event) {
-        this.query = event.target.value;
-        if (this.allMoviesData) {
-            this.moviesData = this.allMoviesData.filter(movie => movie.title.toLowerCase().includes(this.query.toLowerCase()));
-        }
+        this.searchQuery = event.target.value.toLowerCase();
     }
+
+    get filteredMovies() {
+        if (!this.searchQuery) {
+          return this.moviesData;
+        }
+        const query = this.searchQuery.toLowerCase().split(' ').join('%');
+        return this.moviesData.filter(movie => movie.title.toLowerCase().includes(query));
+      }
+      
 }
