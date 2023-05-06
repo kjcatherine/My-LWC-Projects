01d97c0f35b06965c0cfd9c28f01cb389d32c2c3
kate <katejohn967@gmail.com>
2023-05-06 12:20:56 +0100

    added the MapController class and JS file

--- force-app/main/default/lwc/mapsInLwc/mapsInLwc.js
+++ force-app/main/default/lwc/mapsInLwc/mapsInLwc.js
@@ -1,3 +1,39 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,wire } from 'lwc';
+import getAccounts from '@salesforce/apex/MapControllerLwc.getAccounts'
 
-export default class MapsInLwc extends LightningElement {}
+export default class MapsInLwc extends LightningElement {
+    mapMarkers=[]
+    markersTitle="Accounts Location"
+    //To load the map we need the data, and to load te data we need an apex class
+    @wire(getAccounts)
+    wireHandler({data, error}){
+        if(data){
+            console.log(data)
+            this.formatResponse(data)
+        }
+        if(error){
+            console.error(error)
+        }
+    }
+    formatResponse(data){
+        this.mapMarkers = data.map(item=>{
+            return {
+                location:{
+                    Street:item.BillingStreet || '',
+                    City:item.BillingCity || '',
+                    PostalCode:item.PostalCode || '',
+                    State:item.BillingState || '',
+                    Country:item.BillingCountry || '',
+                },
+                icon:'utility:salesforce1',
+                title:item.Name,
+                value:item.Name,
+                description:item.Description
+            }
+        })
+        this.selectedMarker = this.mapMarkers.length && this.mapMarkers[0].value
+    }
+    callMarkerHandler(event){
+        this.selectedMarker = event.detail.selectedMarkerValue
+    }
+}
