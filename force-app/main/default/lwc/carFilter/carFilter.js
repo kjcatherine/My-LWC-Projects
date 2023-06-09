9fcec2dda0c44e88cfb3628ac9630f405044a001
kate <katejohn967@gmail.com>
2023-06-09 12:07:11 +0100

    building layout for carFilter Component

--- force-app/main/default/lwc/carFilter/carFilter.js
+++ force-app/main/default/lwc/carFilter/carFilter.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
 
-export default class CarFilter extends LightningElement {}
+export default class CarFilter extends LightningElement {
+    filters={
+        searchKey:'',
+        maxPrice:999999
+    }
+    handleSearchKeyChange(){
+
+    }
+    handleMaxPriceChange(){
+
+    }
+}
