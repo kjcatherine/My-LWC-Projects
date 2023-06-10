7f6079f0cf264aed66d933e55a7af1be468a37a7
kate <katejohn967@gmail.com>
2023-06-10 12:31:49 +0100

    building the layout for the filter component

--- force-app/main/default/lwc/carFilter/carFilter.js
+++ force-app/main/default/lwc/carFilter/carFilter.js
@@ -1,14 +1,39 @@
-import { LightningElement } from 'lwc';
-
+import { LightningElement, wire } from 'lwc';
+import { getObjectInfo, getPicklistValues } from 'lightning/uiObjectInfoApi';
+import CAR_OBJECT from '@salesforce/schema/Car__c'
+import CATEGORY_FIELD from '@salesforce/schema/Car__c.Category__c'
+import MAKE_FIELD from '@salesforce/schema/Car__c.Make__c'
+//Constants
+const CATEGORY_ERROR = 'error loading categories'
 export default class CarFilter extends LightningElement {
     filters={
         searchKey:'',
         maxPrice:999999
     }
+    categoryError = CATEGORY_ERROR
+    //Fetching Category picklist
+    @wire(getObjectInfo, {objectApiName:CAR_OBJECT})
+    carObjectInfo
+
+    @wire(getPicklistValues, {
+        recordTypeId:'$carObjectInfo.data.defaultRecordTypeId',
+        fieldApiName:CATEGORY_FIELD
+    })categories
+
+    //Fetching Make picklist
+    @wire(getPicklistValues, {
+        recordTypeId:'$carObjectInfo.data.defaultRecordTypeId',
+        fieldApiName:MAKE_FIELD
+    })makeType
+
+
     handleSearchKeyChange(){
 
     }
     handleMaxPriceChange(){
 
+    }
+    handleCheckbox(){
+        
     }
 }
