366290f0886e20594e9a4f3b01bd0b7259ff89f5
kate <katejohn967@gmail.com>
2023-06-13 12:26:53 +0100

    creating the card card component

--- force-app/main/default/lwc/carCard/carCard.js
+++ force-app/main/default/lwc/carCard/carCard.js
@@ -1,3 +1,23 @@
 import { LightningElement } from 'lwc';
+//Car__c Schema
+//import CAR_OBJECT from '@salesforce/schema/Car__c'
+import NAME_FIELD from '@salesforce/schema/Car__c.Name'
+import PICTURE_URL_FIELD from '@salesforce/schema/Car__c.Picture_URL__c'
+import CATEGORY_FIELD from '@salesforce/schema/Car__c.Category__c'
+import MAKE_FIELD from '@salesforce/schema/Car__c.Make__c'
+import MSRP_FIELD from '@salesforce/schema/Car__c.MSRP__c'
+import FUEL_FIELD from '@salesforce/schema/Car__c.Fuel_Type__c'
+import SEATS_FIELD from '@salesforce/schema/Car__c.Seats__c'
+import CONTROL_FIELD from '@salesforce/schema/Car__c.Control__c'
 
-export default class CarCard extends LightningElement {}
+export default class CarCard extends LightningElement {
+    //Exposing feilds to template
+    categoryField = CATEGORY_FIELD
+    makeField = MAKE_FIELD
+    msrpField = MSRP_FIELD
+    fuelField = FUEL_FIELD
+    seatsField = SEATS_FIELD
+    controlField = CONTROL_FIELD
+
+    recordId
+}
