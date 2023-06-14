23184cc1646166a9637d75b7a21234bd29056abb
kate <katejohn967@gmail.com>
2023-06-14 12:52:32 +0100

    building the layout as well as displaying image and title

--- force-app/main/default/lwc/carCard/carCard.js
+++ force-app/main/default/lwc/carCard/carCard.js
@@ -7,9 +7,10 @@ import CATEGORY_FIELD from '@salesforce/schema/Car__c.Category__c'
 import MAKE_FIELD from '@salesforce/schema/Car__c.Make__c'
 import MSRP_FIELD from '@salesforce/schema/Car__c.MSRP__c'
 import FUEL_FIELD from '@salesforce/schema/Car__c.Fuel_Type__c'
-import SEATS_FIELD from '@salesforce/schema/Car__c.Seats__c'
+import SEATS_FIELD from '@salesforce/schema/Car__c.Number_of_Seats__c'
 import CONTROL_FIELD from '@salesforce/schema/Car__c.Control__c'
-
+//Used to extract field values
+import { getFieldValue } from 'lightning/uiRecordApi';
 export default class CarCard extends LightningElement {
     //Exposing feilds to template
     categoryField = CATEGORY_FIELD
@@ -19,5 +20,16 @@ export default class CarCard extends LightningElement {
     seatsField = SEATS_FIELD
     controlField = CONTROL_FIELD
 
-    recordId
+    // Car__c Id
+    recordId = 'a010C0000058IwkQAE'
+
+    //Car fields displayed with specific format
+    carName
+    carPictureUrl
+    handleRecordLoaded(event){
+        const {records} = event.detail
+        const recordData = records[this.recordId]
+        this.carName = getFieldValue(recordData, NAME_FIELD)
+        this.carPictureUrl = getFieldValue(recordData, PICTURE_URL_FIELD)
+    }
 }
