da72463f79249be828477073a756556e016a3c8c
kate <katejohn967@gmail.com>
2023-06-15 13:01:15 +0100

    pulling data using Apex class

--- force-app/main/default/lwc/carTileList/carTileList.js
+++ force-app/main/default/lwc/carTileList/carTileList.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
-
-export default class CarTileList extends LightningElement {}
+import { LightningElement, wire } from 'lwc';
+import getCars from '@salesforce/apex/CarController.getCars'
+export default class CarTileList extends LightningElement {
+    
+    @wire(getCars)
+    carsHandler({data, error}){
+        if(data){
+            console.log(data)
+        }
+        if(error){
+            console.error(error)
+        }
+    }
+}
