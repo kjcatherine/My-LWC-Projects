b3975749aa30e13e0d7d96be7f17594c5bfe56ce
kate <katejohn967@gmail.com>
2023-06-16 13:52:29 +0100

    Creating layout of individual tiles and the list of tiles

--- force-app/main/default/lwc/carTileList/carTileList.js
+++ force-app/main/default/lwc/carTileList/carTileList.js
@@ -1,13 +1,16 @@
 import { LightningElement, wire } from 'lwc';
 import getCars from '@salesforce/apex/CarController.getCars'
 export default class CarTileList extends LightningElement {
-    
+    cars
+    error
     @wire(getCars)
     carsHandler({data, error}){
         if(data){
+            this.cars = data
             console.log(data)
         }
         if(error){
+            this.error(error)
             console.error(error)
         }
     }
