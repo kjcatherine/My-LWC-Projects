a0e5a321c06df1ba626494a43a2f842d01c46153
kate <katejohn967@gmail.com>
2023-06-28 10:00:45 +0100

    logic for fetching and displaying similar cars in a record page

--- force-app/main/default/lwc/similarCars/similarCars.js
+++ force-app/main/default/lwc/similarCars/similarCars.js
@@ -1,7 +1,36 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire} from 'lwc';
+import getSimilarCars from '@salesforce/apex/CarController.getSimilarCars'
+import { getRecord } from 'lightning/uiRecordApi';
+import MAKE_FIELD from '@salesforce/schema/Car__c.Make__c'
 
-export default class SimilarCars extends LightningElement {
+import {NavigationMixin} from 'lightning/navigation'
+export default class SimilarCars extends NavigationMixin(LightningElement) {
+    @api recordId
+    @api objectApiName
+    similarCars
+    @wire(getRecord, {recordId:'$recordId', fields:[MAKE_FIELD]})
+    car
+
     fetchSimilarCars(){
+        getSimilarCars({
+            carId:this.recordId,
+            makeType:this.car.data.fields.Make__c.value
+        }).then(result=>{
+            this.similarCars = result
+            console.log(this.similarCars)
+        }).catch(error=>{
+            console.error(error)
+        })
+    }
 
+    handleViewDetailsClick(event){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes:{
+                recordId: event.target.dataset.Id,
+                objectApiName: this.objectApiName,
+                actionName: 'view'
+            }
+        })
     }
 }
